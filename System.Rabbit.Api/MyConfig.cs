d690b49046a5977f95cbb632f76f501b4e3aac3f
Anderson Junior Rodrigues <andersonkz@ymail.com>
2021-04-11 12:34:38 -0300

    rabbitmq exemple

---
+++ System.Rabbit.Api/MyConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System.Rabbit.Api
+{
+    public class MyConfig
+    {
+        public string ApplicationName { get; set; }
+        public string Version { get; set; }
+    }
+}
